dc070125082af0db30c16b5c97a7eb99f8629eec
Georgios Katsanakis <geo.elgeo@gmail.com>
2021-12-10 10:00:40 +0100

    Added details on QuoteDetails page

--- src/pages/QuoteDetails.js
+++ src/pages/QuoteDetails.js
@@ -1,15 +1,27 @@
 import { Route, useParams } from 'react-router-dom';
 import { Fragment } from 'react';
 import Comments from '../components/comments/Comments';
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
+
+
+const DUMMY_QUOTES = [
+  { id: 'q1', author: 'Kronos', text: 'Learning react is fun!' },
+  { id: 'q2', author: 'Kronos', text: 'Learning react is shit' },
+];
 
 const QuoteDetails = () => {
   const params = useParams();
 
+  const quote = DUMMY_QUOTES.find(quote => quote.id == params.quoteId);
+
+  if (!quote){
+    return <p>No quote found!</p>
+  }
+
   return <Fragment>
-    <h1>Quote Details</h1>
-    <p>{params.quoteId}</p>
+    <HighlightedQuote text={quote.text} author={quote.author}/>
     <Route path={`/quotes/${params.quoteId}/comments`}>
-      <Comments />
+      <Comments/>
     </Route>
   </Fragment>;
 
