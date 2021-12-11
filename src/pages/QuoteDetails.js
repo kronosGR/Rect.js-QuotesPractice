bb734b87f7073f5633bfa03e52ecce29b1a2b480
Georgios Katsanakis <geo.elgeo@gmail.com>
2021-12-11 12:01:42 +0100

    Added link for the quote comments

--- src/pages/QuoteDetails.js
+++ src/pages/QuoteDetails.js
@@ -1,4 +1,4 @@
-import { Route, useParams } from 'react-router-dom';
+import { Link, Route, useParams } from 'react-router-dom';
 import { Fragment } from 'react';
 import Comments from '../components/comments/Comments';
 import HighlightedQuote from '../components/quotes/HighlightedQuote';
@@ -14,15 +14,22 @@ const QuoteDetails = () => {
 
   const quote = DUMMY_QUOTES.find(quote => quote.id == params.quoteId);
 
-  if (!quote){
-    return <p>No quote found!</p>
+  if (!quote) {
+    return <p>No quote found!</p>;
   }
 
   return <Fragment>
     <HighlightedQuote text={quote.text} author={quote.author}/>
+    <Route path={`/quotes/${params.quoteId}/`} exact>
+      <div className="centered">
+        <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>Load
+          Comments</Link>
+      </div>
+    </Route>
     <Route path={`/quotes/${params.quoteId}/comments`}>
       <Comments/>
     </Route>
+
   </Fragment>;
 
 };
