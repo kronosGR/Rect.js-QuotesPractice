40054f49eca03caf1985c4babcea174b594f90a3
Georgios Katsanakis <geo.elgeo@gmail.com>
2021-12-12 12:35:21 +0100

    Added communication for firebase realtime DB, adding and getting quotes

--- src/pages/QuoteDetails.js
+++ src/pages/QuoteDetails.js
@@ -1,26 +1,40 @@
 import { Link, Route, useParams, useRouteMatch } from 'react-router-dom';
-import { Fragment } from 'react';
+import { Fragment, useEffect } from 'react';
 import Comments from '../components/comments/Comments';
 import HighlightedQuote from '../components/quotes/HighlightedQuote';
+import useHttp from '../hooks/use-http';
+import { getSingleQuote } from '../lib/api';
+import LoadingSpinner from '../components/UI/LoadingSpinner';
 
-
-const DUMMY_QUOTES = [
-  { id: 'q1', author: 'Kronos', text: 'Learning react is fun!' },
-  { id: 'q2', author: 'Kronos', text: 'Learning react is shit' },
-];
 
 const QuoteDetails = () => {
   const params = useParams();
   const match = useRouteMatch();
 
-  const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId);
+  const { quoteId } = params;
 
-  if (!quote) {
+  const { sendRequest, status, data: loadedQuote, error } = useHttp(getSingleQuote, true);
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [ sendRequest, quoteId ]);
+
+  if (status === 'pending') {
+    return <div className="centered">
+      <LoadingSpinner/>
+    </div>;
+  }
+
+  if (error) {
+    return <p className='centered'>{error}</p>
+  }
+
+  if (!loadedQuote.text) {
     return <p>No quote found!</p>;
   }
 
   return <Fragment>
-    <HighlightedQuote text={quote.text} author={quote.author}/>
+    <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author}/>
     <Route path={match.path} exact>
       <div className="centered">
         <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>Load
